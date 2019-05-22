a389d41b2b607d2c4c9e9f0c33f24c04bcedd17b
NelsonKioko <kiokonelson2@gmail.com>
2019-05-22 19:00:13 +0300

    create a forms.py file in Insta app

--- Insta/models.py
+++ Insta/models.py
@@ -15,3 +15,18 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('Insta-home')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Insta.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
