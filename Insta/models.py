eca6fb2cc4c5c096874d314b76bd66300f9c892e
NelsonKioko <kiokonelson2@gmail.com>
2019-05-22 12:44:53 +0300

    add a location field

--- Insta/models.py
+++ Insta/models.py
@@ -5,13 +5,13 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
     content = models.ImageField(upload_to='timeline_pics')
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.location
 
     def get_absolute_url(self):
         return reverse('Insta-home')
--- Insta/views.py
+++ Insta/views.py
@@ -44,7 +44,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['location', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -53,7 +53,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['location', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
